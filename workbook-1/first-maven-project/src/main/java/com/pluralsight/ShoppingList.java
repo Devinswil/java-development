ded495657b7a0df975107c20e4c1d06e2c7cf520
DevinW <devinswilson03@gmail.com>
2024-09-25 13:31:34 -0400

    initial commit

---
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,17 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("xbox");
+        System.out.println("playstation");
+        System.out.println("apples");
+        System.out.println("notebooks");
+        System.out.println("iphone");
+        System.out.println("pen");
+        System.out.println("tv");
+        System.out.println("Fruit Snacks");
+        System.out.println("water bottles");
+        System.out.println("papers");
+
+    }
+}
