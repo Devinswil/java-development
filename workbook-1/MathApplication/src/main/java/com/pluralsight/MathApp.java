fe205db9244ff12d27b8bd152c90cc4dd4d57157
DevinW <devinswilson03@gmail.com>
2024-09-26 13:44:52 -0400

    Updated code to display better

--- workbook-1/MathApplication/src/main/java/com/pluralsight/MathApp.java
+++ workbook-1/MathApplication/src/main/java/com/pluralsight/MathApp.java
@@ -5,19 +5,22 @@ public class MathApp {
         float bobSalary = 20_000f;
         float garySalary = 40_000f;
         double highestSalary = Math.max(bobSalary, garySalary);
-        System.out.println("The highest salary is" + highestSalary);
+        System.out.println("The highest salary is " + highestSalary);
 
         int carprice = 10000;
         int truckprice = 30000;
-        System.out.println(Math.min(carprice, truckprice));
+        double smallestPrice = Math.min(carprice, truckprice);
+        System.out.println("The Smallest salary is " + smallestPrice);
+
 
         double radius = 7.25;
         double area = Math.PI * Math.pow(radius, 2);
-        System.out.println(area);
+        System.out.println("The area of the circle is " + area);
+
 
         double number = 5.0;
         double root = Math.sqrt(number);
-        System.out.println("The Square root of 5.0 is" + root);
+        System.out.println("The Square root of 5.0 is " + root);
 
         double x1 = 5;
         double x2 = 85;
@@ -25,13 +28,14 @@ public class MathApp {
         double y2 = 50;
 
        double distance = Math.sqrt((Math.pow(x2 - x1, 2)+Math.pow(y2 - y1, 2)));
-        System.out.println(distance);
+        System.out.println("The distance between the two points is " +  distance);
 
         double negNumber = -3.8;
         double absNumber = Math.abs(negNumber);
-        System.out.println("The absloute number is"+ negNumber);
+        System.out.println("The absolute number is "+ negNumber);
 
-        System.out.println(Math.random());
+        double rdmNumber = Math.random();
+        System.out.println("The random number is " + rdmNumber);
 
 
 
