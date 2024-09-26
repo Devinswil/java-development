054712167ca8429f7f7bbcfacf91b3fc90e18085
DevinW <devinswilson03@gmail.com>
2024-09-26 16:26:47 -0400

    made it user friendly

--- workbook-1/PayrollCalculator/src/main/java/PayrollCalculator.java
+++ workbook-1/PayrollCalculator/src/main/java/PayrollCalculator.java
@@ -7,12 +7,18 @@ public class PayrollCalculator {
 
         System.out.println("Enter your name");
         String name = myscan.nextLine();
-        System.out.println("Hours worked this Pay Period?");
+        System.out.println("Hours worked this Pay Period? <--Bi-weekly");
         double hours = myscan.nextDouble();
         System.out.println("Enter payrate");
         double payrate = myscan.nextDouble();
         double grosspay = hours * payrate;
+
+
+
         System.out.println("Gross pay " + grosspay);
+        double yearlySalary = ((grosspay * 2) * 12);
+        System.out.println("Projected Yearly Salary  "+ yearlySalary);
+
 
 
     }
