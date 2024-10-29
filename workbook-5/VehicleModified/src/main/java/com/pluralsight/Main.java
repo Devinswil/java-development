5e682e6c2428f70df6c31e67592b19380ce285ca
DevinW <devinswilson03@gmail.com>
2024-10-29 09:52:08 -0400

    made construtors for classes.

---
+++ workbook-5/VehicleModified/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,6 @@
+package com.pluralsight;
+
+public class Main {
+    Hovercraft hovercraft= new Hovercraft("fast","blue",300,50,2,40,100);
+     Car car = new Car("bendz","black",20,20,20,2,1);
+}
--- workbook-5/Vehicles/src/main/java/pluralsight/Main.java
+++ workbook-5/Vehicles/src/main/java/pluralsight/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
         Car car = new Car();
         car.closeTrunk();
-        car
+
 
 
     }
