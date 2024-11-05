b81de1bd6e741313e7887a2dbc079c7abae3e73f
DevinW <devinswilson03@gmail.com>
2024-11-05 11:04:30 -0500

    added methods

--- workbook-6/Streams/src/main/java/com/pluralsight/Interface.java
+++ workbook-6/Streams/src/main/java/com/pluralsight/Interface.java
@@ -20,39 +20,62 @@ public class Interface {
 
 
         findName(names);
-       int average = getAgeAverage(names);
+        int average = getAgeAverage(names);
         System.out.println(average);
-
+        getOldest(names);
+        System.out.println(names);
+        getYoungest(names);
+        System.out.println(names);
 
 
     }
 
     public static void findName(List<Person> names) {
         Scanner myscan = new Scanner(System.in);
-        List<Person> thisPersonList;
         System.out.println("What name would you like to Search for?");
         String nameToFind = myscan.nextLine();
         boolean found = false;
         for (Person name : names) {
             if (nameToFind.equalsIgnoreCase(name.getLastName()) || nameToFind.equalsIgnoreCase(name.getFirstName())) {
                 System.out.println(name.getFirstName() + " " + name.getLastName());
-                found=true;
+                found = true;
 
             }
         }
-        if (!found){
+        if (!found) {
             System.out.println("Name not found!");
         }
 
     }
-   public static int getAgeAverage(List<Person> names){
-       int sum=0;
-       for (Person name : names) {
-           sum += name.getAge();
+
+    public static int getAgeAverage(List<Person> names) {
+        int sum = 0;
+        for (Person name : names) {
+            sum += name.getAge();
 
 
+        }
+        return sum / names.size();
+    }
 
-       }
-       return sum/ names.size();
-   }
+    public static int getOldest(List<Person> names) {
+        int maxAge = Integer.MIN_VALUE;
+        for (Person name : names) {
+            if (name.getAge() > maxAge) {
+                maxAge = name.getAge();
+            }
+
+        }
+        return maxAge;
+    }
+    public static int getYoungest(List<Person> names) {
+        int minAge = Integer.MAX_VALUE;
+        for (Person name : names) {
+            if (name.getAge() < minAge) {
+                minAge = name.getAge();
+            }
+
+        }
+        return minAge;
+    }
 }
