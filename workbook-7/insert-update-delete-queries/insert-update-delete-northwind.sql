a0429e685542f43aa33a56122ffdea5e00e2818c
DevinW <devinswilson03@gmail.com>
2024-11-22 10:38:44 -0500

    first commit on workshop

---
+++ workbook-7/insert-update-delete-queries/insert-update-delete-northwind.sql
@@ -0,0 +1,18 @@
+-- 1
+INSERT INTO suppliers
+(SupplierID,CompanyName,ContactName,ContactTitle,Address,City,Region,PostalCode,Country,Phone,Fax,HomePage)
+VALUES
+(4512,'DEV inc.','DEVIN WILSON','LEASE','downtown','pittsburgh','pa','14514','united states','4126658745','4125','google');
+
+
+-- 2
+INSERT INTO products (ProductID, ProductName, SupplierID, CategoryID, UnitPrice)
+VALUES (78,'Ropes', 75,874, 5.0);
+
+-- 3
+-- 4
+-- 5
+-- 6
+-- 7
+-- 8
+-- 9
