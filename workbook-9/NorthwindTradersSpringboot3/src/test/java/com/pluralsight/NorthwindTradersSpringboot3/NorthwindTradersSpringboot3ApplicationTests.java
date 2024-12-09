1cc2642239d67dfb0e22f8496baa053fb5ca8210
DevinW <devinswilson03@gmail.com>
2024-12-09 13:17:20 -0500

    had trouble setting up project needs updated

---
+++ workbook-9/NorthwindTradersSpringboot3/src/test/java/com/pluralsight/NorthwindTradersSpringboot3/NorthwindTradersSpringboot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.NorthwindTradersSpringboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorthwindTradersSpringboot3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
